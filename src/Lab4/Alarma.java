ffbbdba78ce4387622a542440bfd75b0cafda746
Ana <Ana@10.159.226.219>
2017-10-13 09:04:01 -0600

    9:03 am

---
+++ src/Lab4/Alarma.java
@@ -0,0 +1,37 @@
+
+/**
+ *
+ * @author Ana Lucia Hernandez. 17138
+ * @since 13, Oct, 2017
+ * @purpose modelar la informacion extra de una alarma
+ * Alarma.java
+ * 
+ */
+package Lab4;
+
+public class Alarma extends Timbre 
+{
+/**
+ *
+ * Metodo para imprimir "alarma 1"
+ */
+    public void m1() {
+		System.out.println("alarma 1");
+	}
+/**
+ *
+ * Metodo para imprimir el "timbre 2" de la clase padre
+ * @override al metodo de la clase padre
+ */
+	 
+	public void m2() {
+		super.m1();
+	}
+/**
+ *
+ * Metodo para imprimir el objeto
+ */	 
+	public String toString() {
+		return super.toString() + "..." + super.toString();
+	}
+}
---
+++ src/Lab4/AlarmaMain.java
@@ -0,0 +1,19 @@
+
+/**
+ *
+ * @author Ana Lucia Hernandez. 17138
+ * @since 13, Oct, 2017
+ * @purpose clase main
+ * AlarmaMain.java
+ * 
+ */
+package Lab4;
+
+public class AlarmaMain {
+	public static void main(String[] args) {
+		AlarmaEmergencia alarmaIncendio = new AlarmaEmergencia();
+		alarmaIncendio.m1();                  // emergencia 1
+		alarmaIncendio.m2();                  // alarma 1 / timbre 1
+		System.out.println(alarmaIncendio);   // emergencia Ringâ€¦Ringâ€¦Ringâ€¦Ring
+	}
+}
